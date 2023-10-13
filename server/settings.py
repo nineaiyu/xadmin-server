d5396b8a104cc0c28ffb178bff4ec1943e5a7b13
isummer <nineven@qq.com>
2023-10-13 13:07:16 +0800

    perf: 优化菜单国际化展示

--- server/settings.py
+++ server/settings.py
@@ -239,7 +239,7 @@ REST_FRAMEWORK_EXTENSIONS = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(seconds=3600),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=15),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': True,  # 在登录的时候更新user表  last_login 字段
