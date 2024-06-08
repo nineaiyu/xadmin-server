fa94276f44bcd4bff01bf252f9877bfde8e1ef7e
isummer <nineven@qq.com>
2024-06-08 13:05:07 +0800

    feat: 增加资源ID缓存文档

--- common/api/common.py
+++ common/api/common.py
@@ -6,6 +6,8 @@
 # date : 6/7/2024
 import uuid
 
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.views import APIView
 
 from common.cache.storage import CommonResourceIDsCache
@@ -13,8 +15,15 @@ from common.core.response import ApiResponse
 
 
 class ResourcesIDCacheApi(APIView):
+    """资源ID 缓存"""
     permission_classes = []
 
+    @swagger_auto_schema(request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        required=['resources'],
+        properties={'resources': openapi.Schema(description='主键列表', type=openapi.TYPE_ARRAY,
+                                          items=openapi.Schema(type=openapi.TYPE_STRING))}
+    ), operation_description='将资源数据临时保存到服务器')
     def post(self, request, *args, **kwargs):
         spm = str(uuid.uuid4())
         resources = request.data.get('resources')
