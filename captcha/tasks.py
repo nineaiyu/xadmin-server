80b267f1aadd6b2c919bf3f6337b5c8939a47b83
isummer <nineven@qq.com>
2024-10-09 13:07:31 +0800

    fix: 修复定时任务异常

--- captcha/tasks.py
+++ captcha/tasks.py
@@ -4,10 +4,13 @@
 # filename : tasks
 # author : ly_13
 # date : 9/15/2024
+from celery import shared_task
+
 from captcha.models import CaptchaStore
 from common.celery.decorator import register_as_period_task
 
 
+@shared_task
 @register_as_period_task(crontab='12 2 * * *')
 def auto_clean_expired_captcha_job():
     CaptchaStore.remove_expired()
