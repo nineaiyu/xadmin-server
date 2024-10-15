3308d082fc3010e452a0aa843e28a2616f98e4fa
isummer <ly_1301b@163.com>
2024-10-15 12:40:28 +0800

    feat: vscode容器化开发

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,6 +18,9 @@ if [[ "${action:0:1}" == "/" ]];then
     $@
 elif [[ "$action" == "bash" || "$action" == "sh" ]];then
     bash
+elif [[ "$action" == "sleep" ]];then
+    echo "Sleep 365 days"
+    sleep 365d
 else
     python manage.py "${action}" "${service}"
 fi
