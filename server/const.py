92554dfd985ec043e329651dd4c0917d1f2445e9
isummer <nineven@qq.com>
2024-11-15 14:01:07 +0800

    perf: v

--- server/const.py
+++ server/const.py
@@ -10,5 +10,5 @@ PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 LOG_DIR = os.path.join(PROJECT_DIR, "data", "logs")
 TMP_DIR = os.path.join(PROJECT_DIR, "tmp")
 CELERY_LOG_DIR = os.path.join(LOG_DIR, "task")
-VERSION = '4.2.0'
+VERSION = '4.2.1'
 CONFIG = ConfigManager.load_user_config()
